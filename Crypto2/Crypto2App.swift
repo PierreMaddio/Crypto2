1f0d24f10aced9d8de28f2ec878c015d6908f3a5
PierreMaddio <pmsymf@gmail.com>
2022-09-19 23:54:12 +0200

    first commit

---
+++ Crypto2/Crypto2App.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto2App.swift
+//  Crypto2
+//
+//  Created by Pierre on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
